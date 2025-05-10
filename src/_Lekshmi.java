58050fc3225f9912a4b344a55b17b2fd2e3ed44f
lekshmips07 <lekshmips07@gmail.com.com>
2025-05-10 22:06:20 +0530

    05/10

---
+++ src/_Lekshmi.java
@@ -0,0 +1,8 @@
+public class _Lekshmi {
+    public static void main(String[] args) {
+        System.out.println("Commit test");
+        System.out.println(5+5);
+        System.out.println("5+5");
+        System.out.println(5/2);
+    }
+}
