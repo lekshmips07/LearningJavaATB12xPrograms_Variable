761af283956309a3e10dcbd0ba7fbb812651cb92
lekshmips07 <lekshmips07@gmail.com.com>
2025-05-20 23:11:00 +0530

    Multiplication_Table

---
+++ src/multiplication_Table_5.java
@@ -0,0 +1,25 @@
+public class multiplication_Table_5 {
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.printf("5x1=%d",num);
+        System.out.println();
+        System.out.printf("5x2=%d",num*2);
+        System.out.println();
+        System.out.printf("5x3=%d",num*3);
+        System.out.println();
+        System.out.printf("5x4=%d",num*4);
+        System.out.println();
+        System.out.printf("5x5=%d",num*5);
+        System.out.println();
+        System.out.printf("5x6=%d",num*6);
+        System.out.println();
+        System.out.printf("5x7=%d",num*7);
+        System.out.println();
+        System.out.printf("5x8=%d",num*8);
+        System.out.println();
+        System.out.printf("5x9=%d",num*9);
+        System.out.println();
+        System.out.printf("5x10=%d",num*10);
+
+    }
+}
